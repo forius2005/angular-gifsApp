4a9703a2d4ef1abf51ab7b7138cbf318c0b33d17
Sebastian Ciprian <forius2005@gmail.com>
2024-12-20 11:50:34 -0500

    Second commit

---
+++ src/app/gifs/components/gifs-card/gifs-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-gifs-card',
+  standalone: false,
+
+  templateUrl: './gifs-card.component.html'
+})
+export class GifsCardComponent implements OnInit {
+
+  @Input()
+  public gif!: Gif;
+
+
+  ngOnInit(): void {
+
+    if ( !this.gif ) throw new Error('Gif property is required!');
+
+  }
+
+}
